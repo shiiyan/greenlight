ff9d68ea5eaa83c30d470afd9e39181485e3e860
shiiyan <shiiyan79@gmail.com>
2025-06-08 23:33:49 +0900

    feat: create validator

---
+++ internal/validator/validator.go
@@ -0,0 +1,29 @@
+package validator
+
+type Validator struct {
+	Errors map[string]string
+}
+
+func New() *Validator {
+	return &Validator{
+		Errors: make(map[string]string),
+	}
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
+
+func (v *Validator) AddError(key, message string) {
+	if existing := v.Errors[key]; existing != "" {
+		message = existing + ", " + message
+	}
+
+	v.Errors[key] = message
+}
